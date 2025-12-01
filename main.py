40270102aab39896813c760856a3df96156625c6
Cauanz <61758981+Cauanz@users.noreply.github.com>
2025-12-01 11:39:10 -0300

    🚧 refatora lógica de exibição de músicas para identificar duplicatas e melhora o formulário de upload de pastas

--- main.py
+++ main.py
@@ -7,12 +7,23 @@ folder = []
 
 @app.route('/')
 def index():
-  # TODO - TAMBÉM APRENDER A CARREGAR PASTA E LER ARQUIVOS
   songs = []
-  songs = folder
+  seen_hashes = set()
+
+  for song in folder:
+    hash = song['hash']
+
+    if hash in seen_hashes:
+      song['status'] = 'Duplicated'
+      songs.append(song)
+      continue
+
+    seen_hashes.add(hash)
+    songs.append(song)
+  # TODO - CONTINUAR ESSE FILTRO (PROVAVELMENTE VAI FICAR MAIOR GANHANDO E ADICIONANDO FUNÇÕES PARA EXCLUIR DUPLICATAS (NO FRONTEND), ETC...)
   return render_template('home.html', songs=songs)
 
-@app.route('/check-folder', methods=["GET", "POST"])
+@app.route('/check-folder', methods=["POST"])
 def check_folder():
 
   temp_files = []
@@ -20,17 +31,22 @@ def check_folder():
 
   if request.method == 'POST':
     files = request.files.getlist('selectfolder')
-    for file in files:
-      temp_files.append(file)
 
-      # file.read() # RETORNA OS BYTES DO ARQUIVO
-      newFile = {}
-      newFile['pathname'] = file.filename.split('/')[0]
-      newFile['filename'] = file.filename.split('/')[1]
-      newFile['hash'] = hash_song(file)
-      newFile['status'] = 'Mapped'
+    try:
+      for file in files:
+        temp_files.append(file)
 
-      folder.append(newFile)
+        # file.read() # RETORNA OS BYTES DO ARQUIVO
+        newFile = {}
+        newFile['pathname'] = file.filename.split('/')[0]
+        newFile['filename'] = file.filename.split('/')[1]
+        newFile['hash'] = hash_song(file)
+        newFile['status'] = 'Mapped'
+
+        folder.append(newFile)
+    except:
+      print('An exception occurred')
+
     
   return redirect('/')
 # TODO - CONTINUAR DESCOBRINDO COMO RECEBER A PASTA/ARQUIVOS, SALVAR ELES LOCALMENTE (TALVEZ PRECISE DE UM DB SIMPLES) E DAI FAZER AS COISAS
---
+++ static/scripts/index.js
@@ -0,0 +1,11 @@
+
+// let previousPageBtn = document.querySelector("#back")
+
+// TODO - TERMINAR ISSO DEPOIS
+// previousPageBtn.addEventListener('click', (e) => {
+//   const form = document.querySelector('#folderForm');
+//   const table = document.querySelector("#table");
+
+//   form.style.display = 'flex';
+//   table.style.display = "None";
+// })
